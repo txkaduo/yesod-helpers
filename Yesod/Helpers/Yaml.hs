3aa36411145dcdacfdaa0e09c5d1c284fb85f4ef
Gilbert Fine <gilbert.fine@gmail.com>
2014-08-30 11:50:36 +0800

    new helper function for implement simple ACL by config file

---
+++ Yesod/Helpers/Yaml.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Yesod.Helpers.Yaml where
+
+import Prelude
+import Data.Yaml
+import Yesod
+import Data.Text                            (Text)
+import Data.Aeson                           (withObject)
+import qualified Data.Text                  as T
+import Data.Maybe                           (isJust)
+import Data.List                            (find)
+
+import Yesod.Helpers.Aeson
+
+
+-- | check whether a string is in the list identified by a key string
+-- some special strings:
+-- __any__ will match any string
+--
+-- example yaml like this
+-- Develpment:
+--   dangerous-op: foo bar
+--   simple-op:
+--     - foo
+--     - bar
+--   safe-op: __any__
+checkInListYaml ::
+    (Show config, MonadIO m, MonadLogger m) =>
+    FilePath
+    -> config       -- ^ config section: Develpment/Production/...
+    -> Text         -- ^ the key
+    -> Text         -- ^ the name looking for
+    -> m Bool
+checkInListYaml fp c key name = do
+    result <- liftIO $ decodeFileEither fp
+    case result of
+        Left ex -> do
+                    $(logError) $ T.pack $
+                        "failed to parse YAML file " ++ show fp ++ ": " ++ show ex
+                    return False
+        Right v -> do
+                    case parseEither look_for_names v of
+                        Left err -> do
+                                $(logError) $ T.pack $
+                                    "YAML " ++ show fp
+                                            ++ " contains invalid content: "
+                                            ++ show err
+                                return False
+                        Right names -> return $ isJust $ find (match name) names
+    where
+        look_for_names = withObject "section-mapping" $ \obj -> do
+            obj .:? (T.pack $ show c)
+                >>= maybe (return [])
+                    (\section -> do
+                        section .:? key >>=
+                                    maybe (return []) (parseWordList' "words")
+                    )
+
+        match y x = if x == "__any__"
+                        then True
+                        else y == x
