129d3a0e7285f51855a7f785ec39387fd71d36fc
lei.zhong <lei.zhong@yoo-e.com>
2014-09-19 16:30:33 +0800

    Add some function to show field path in error info.
    Now the error info will show field path like "files/[0]/instances/[0]/version"

--- Yesod/Helpers/Aeson.hs
+++ Yesod/Helpers/Aeson.hs
@@ -8,6 +8,10 @@ import qualified Data.Aeson             as A
 import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString        as B
 import qualified Text.Parsec            as PC
+import qualified Control.Monad.Trans.Reader as R
+import qualified Control.Monad.Trans.State as S
+import Control.Monad.Trans              (lift)
+import Data.List                        (intersperse)
 
 import Text.Parsec.Text                 ()
 import Data.Text                        (Text)
@@ -17,6 +21,7 @@ import Data.List                        (find)
 import Data.ByteString                  (ByteString)
 import Text.Parsec                      (ParsecT)
 import Data.Functor.Identity            (Identity)
+import Data.Maybe                       (fromMaybe)
 
 import Yesod.Helpers.Parsec             (splitByParsec)
 
@@ -27,6 +32,97 @@ nullToNothing (Just v)    = parseJSON v
 
 (.:?*) :: (FromJSON a) => Object -> Text -> Parser (Maybe a)
 obj .:?* key = obj .:? key >>= nullToNothing
+
+{-
+-- | combines (.:) and (>>=) for better error reporting
+parseReqSubField :: FromJSON a => Object -> Text -> (a -> Parser b) -> Parser b
+parseReqSubField obj field p = obj .: field >>= modifyFailure report . p
+    where
+        report s = "Failed to parse field '" ++ (T.unpack field) ++ "': " ++ s
+-}
+
+type SubFieldParser a = S.StateT [Text] Parser a
+
+-- | run action of Parser but modify its error message to report
+-- where the error was in (path of fields)
+reportFieldPathParser :: Parser a -> SubFieldParser a
+reportFieldPathParser p = do
+    fields <- S.get
+    let report s =
+            if null fields
+                then s
+                else "in field path '"
+                        ++ (T.unpack $ T.concat $ intersperse (T.singleton '/') $
+                                reverse fields)
+                        ++ "': " ++ s
+    lift $ modifyFailure report p
+
+freezeField :: SubFieldParser a -> SubFieldParser a
+freezeField p = do
+    s <- S.get
+    lift $ S.evalStateT p s
+
+withObjectS :: String -> (Object -> SubFieldParser a) -> Value -> SubFieldParser a
+withObjectS = liftWithS withObject
+
+liftWithS ::
+    (String -> (b -> Parser a) -> Value -> Parser a)
+    -> String -> (b -> SubFieldParser a) -> Value -> SubFieldParser a
+liftWithS wf expected p v = do
+    ks <- S.get
+    lift $ wf expected (flip S.evalStateT ks . p) v
+
+
+atField :: FromJSON a =>
+    (Object -> Text -> Parser a)
+    -> Object
+    -> Text
+    -> (a -> Parser b)
+    -> SubFieldParser b
+atField getf obj key p = do
+    ks <- S.get
+    lift $ S.evalStateT (reportFieldPathParser $ getf obj key >>= p) (key : ks)
+
+atFieldS :: FromJSON a =>
+    (Object -> Text -> Parser a)
+    -> Object
+    -> Text
+    -> (a -> SubFieldParser b)
+    -> SubFieldParser b
+atFieldS getf obj key p = do
+    ks <- S.get
+    lift $ S.evalStateT ((reportFieldPathParser $ getf obj key) >>= p) (key : ks)
+
+reqField :: FromJSON a => Object -> Text -> (a -> Parser b) -> SubFieldParser b
+reqField = atField (.:)
+
+reqFieldS :: FromJSON a => Object -> Text -> (a -> SubFieldParser b) -> SubFieldParser b
+reqFieldS = atFieldS (.:)
+
+optField :: FromJSON a => Object -> Text -> (Maybe a -> Parser b) -> SubFieldParser b
+optField obj key p = do
+    ks <- S.get
+    lift $ S.evalStateT (reportFieldPathParser $ obj .:? key >>= p) (key : ks)
+
+optFieldS :: FromJSON a => Object -> Text -> (Maybe a -> SubFieldParser b) -> SubFieldParser b
+optFieldS obj key p = do
+    ks <- S.get
+    lift $ S.evalStateT ((reportFieldPathParser $ obj .:? key) >>= p) (key : ks)
+
+(+.:) :: (FromJSON a) => Object -> Text -> SubFieldParser a
+obj +.: key = atField (.:) obj key return
+
+(+.:?) :: (FromJSON a) => Object -> Text -> SubFieldParser (Maybe a)
+obj +.:? key = atField (.:?) obj key return
+
+(+.:?*) :: (FromJSON a) => Object -> Text -> SubFieldParser (Maybe a)
+obj +.:?* key = atField (.:?*) obj key return
+
+(+.!=) :: SubFieldParser (Maybe a) -> a -> SubFieldParser a
+p +.!= x = fmap (fromMaybe x) p
+
+runSubFieldParser :: [Text] -> SubFieldParser a -> Parser a
+runSubFieldParser ks sp = S.evalStateT sp $ reverse ks
 
 -- | Parse a string (usually a word), with a lookup table.
 lookupParseText :: String -> [([Text], a)] -> Text -> Parser a
@@ -93,6 +189,12 @@ parseArray ::
     -> Value -> Parser [a]
 parseArray type_name f = withArray type_name $  parseList type_name f . V.toList
 
+parseArrayS ::
+    String                          -- ^ the type name when reporting error
+    -> (Value -> SubFieldParser a)          -- ^ function to apply on each of array
+    -> Value -> SubFieldParser [a]
+parseArrayS type_name f = liftWithS withArray type_name $ parseListS f . V.toList
+
 parseList ::
     String                   -- ^ the type name when reporting error
     -> (Value -> Parser a)         -- ^ function to apply on each of array
@@ -106,7 +208,15 @@ parseList type_name f = mapM f' . zip [0..]
                         ++ show (idx :: Int)
                         ++ " as expected type '" ++ type_name ++ "': "
                         ++ s
-
+parseListS ::
+    (Value -> SubFieldParser a)         -- ^ function to apply on each of array
+    -> [Value] -> SubFieldParser [a]
+parseListS f = mapM f' . zip [(0::Int)..]
+    where
+        f' (idx, v) = do
+            ks <- S.get
+            let new_key = T.pack $ concat $ [ "[", show idx, "]" ]
+            lift $ S.evalStateT (f v) (new_key : ks)
 
 reportExpected :: String -> Parser a -> Parser a
 reportExpected expected f = modifyFailure report f
