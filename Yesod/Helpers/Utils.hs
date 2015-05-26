0bcf8e17f68053dae99738c01179c744b5db6731
Gilbert Fine <gilbert.fine@gmail.com>
2015-05-27 00:39:47 +0800

    new functions to convert some full-width char to half-width

--- Yesod/Helpers/Utils.hs
+++ Yesod/Helpers/Utils.hs
@@ -6,4 +6,21 @@ module Yesod.Helpers.Utils
     , module Yesod.Helpers.Logger
     ) where
 
+import Prelude
+import Data.Char
 import Yesod.Helpers.Logger
+
+toHalfWidthEnglishAlpha :: Char -> Char
+toHalfWidthEnglishAlpha ch
+    | ch >= 'Ａ' && ch <= 'Ｚ'  = chr $ ord 'A' + ord ch - ord 'Ａ'
+    | ch >= 'ａ' && ch <= 'ｚ'  = chr $ ord 'a' + ord ch - ord 'ａ'
+    | otherwise                 = ch
+
+
+toHalfWidthDigit :: Char -> Char
+toHalfWidthDigit ch
+    | ch >= '０' && ch <= '９'  = chr $ ord '0' + ord ch - ord '０'
+    | otherwise                 = ch
+
+toHalfWidthEnglishAlphaDigit :: Char -> Char
+toHalfWidthEnglishAlphaDigit = toHalfWidthEnglishAlpha . toHalfWidthDigit
