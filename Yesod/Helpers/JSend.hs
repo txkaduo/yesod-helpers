ddcc9c7c611a1be91d84a324e3e36f81696d1101
Gilbert Fine <gilbert.fine@gmail.com>
2015-10-27 18:39:49 +0800

    simple tools for JSend

---
+++ Yesod/Helpers/JSend.hs
@@ -0,0 +1,30 @@
+{-|
+ Tools for output JSON message in JSend
+-}
+{-# LANGUAGE OverloadedStrings #-}
+module Yesod.Helpers.JSend where
+
+import Yesod
+import Data.Text                            (Text)
+import Data.Maybe
+
+data JSendMsg = JSendSuccess Value
+                | JSendFail Value
+                | JSendError Text (Maybe Integer) (Maybe Value)
+                    -- ^ message, code, data
+                deriving (Show, Read, Eq)
+
+
+instance ToJSON JSendMsg where
+    toJSON (JSendSuccess dat) = object  [ "status" .= ("success" :: Text)
+                                        , "data" .= dat
+                                        ]
+    toJSON (JSendFail dat)  = object [ "status" .= ("fail" :: Text)
+                                     , "data" .= dat
+                                     ]
+    toJSON (JSendError err_msg m_code m_dat) = object $ catMaybes
+                                                [ Just $ "status" .= ("error" :: Text)
+                                                , Just $ "message" .= err_msg
+                                                , fmap ("data" .= ) m_dat
+                                                , fmap ("code" .= ) m_code
+                                                ]
