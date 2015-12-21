884f0578d52aa6c01116055ada19893560a2f33e
Gilbert Fine <gilbert.fine@gmail.com>
2015-12-21 12:53:59 +0800

    add tools for settings of session cookie

---
+++ Yesod/Helpers/Session.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Yesod.Helpers.Session where
+
+import Prelude
+import Control.Applicative
+import Yesod
+import Data.String                          (fromString)
+import Data.ByteString                      (ByteString)
+import qualified Data.ByteString            as B
+import Data.Aeson                           (withObject, (.:?))
+import Data.Time                            (DiffTime)
+import Web.Cookie
+
+
+-- | for use in settings
+data SessionCookieSettings = SessionCookieSettings
+                                { sessCookieSettingsDomain  :: Maybe ByteString
+                                , sessCookieSettingsName    :: Maybe ByteString
+                                , sessCookieSettingsPath    :: Maybe ByteString
+                                , sessCookieSettingsMaxAge  :: Maybe DiffTime
+                                }
+                                deriving (Show)
+
+instance FromJSON SessionCookieSettings where
+    parseJSON = withObject "SessionCookieSettings" $ \o -> do
+                    SessionCookieSettings <$> (fmap fromString <$> o .:? "domain")
+                                        <*> (fmap fromString <$> o .:? "name")
+                                        <*> (fmap fromString <$> o .:? "path")
+                                        <*> (fmap (fromIntegral :: Int -> DiffTime) <$> o .:? "max-age")
+
+
+amendSessionCookie :: SessionCookieSettings -> SetCookie -> SetCookie
+amendSessionCookie settings = add_domain . add_name . add_path . add_max_age
+    where
+        add_domain ck = case sessCookieSettingsDomain settings of
+                            Just n | not (B.null n)   -> ck { setCookieDomain = Just n }
+                            _                       -> ck
+        add_name ck = case sessCookieSettingsName settings of
+                            Just n | not (B.null n)   -> ck { setCookieName = n }
+                            _                       -> ck
+        add_path ck = case sessCookieSettingsPath settings of
+                            Just n | not (B.null n)   -> ck { setCookiePath = Just n }
+                            _                       -> ck
+        add_max_age ck = ck { setCookieMaxAge = sessCookieSettingsMaxAge settings <|> setCookieMaxAge ck }
