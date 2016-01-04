9022212d03ab0b988be046b83336196091910955
Gilbert Fine <gilbert.fine@gmail.com>
2016-01-04 22:31:00 +0800

    add new module: Jquery

---
+++ Yesod/Helpers/Jquery.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Yesod.Helpers.Jquery where
+
+import Prelude
+import Data.Text                            (Text)
+
+
+data BootcssCdn = BootcssCdn Bool
+
+class JqueryCdnUrl a where
+    urlJqueryJsText :: a -> Text
+
+    urlJqueryUiJsText :: a -> Text
+
+    urlJqueryUiCssText :: a -> Text
+
+
+instance JqueryCdnUrl BootcssCdn where
+    urlJqueryJsText (BootcssCdn min_ver) =
+        if min_ver
+            then "//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"
+            else "//cdn.bootcss.com/jquery/1.11.3/jquery.js"
+
+    urlJqueryUiJsText (BootcssCdn min_ver) =
+        if min_ver
+            then "//cdn.bootcss.com/jqueryui/1.11.4/jquery-ui.min.js"
+            else "//cdn.bootcss.com/jqueryui/1.11.4/jquery-ui.js"
+
+    urlJqueryUiCssText (BootcssCdn min_ver) =
+        if min_ver
+            then "//cdn.bootcss.com/jqueryui/1.11.4/jquery-ui.min.css"
+            else "//cdn.bootcss.com/jqueryui/1.11.4/jquery-ui.css"
