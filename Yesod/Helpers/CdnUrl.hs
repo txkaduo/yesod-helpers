8809cdcace84efc4938e305f0e921857bd8bc0e2
Gilbert Fine <gilbert.fine@gmail.com>
2016-02-01 13:16:44 +0800

    some new classes for defining urls of famouse js/css

--- Yesod/Helpers/CdnUrl.hs
+++ Yesod/Helpers/CdnUrl.hs
@@ -5,8 +5,6 @@ import Prelude
 import Data.Text                            (Text)
 
 
-data BootcssCdn = BootcssCdn Bool
-
 class JqueryCdnUrl a where
     urlJqueryJsText :: a -> Text
 
@@ -14,6 +12,18 @@ class JqueryCdnUrl a where
 
     urlJqueryUiCssText :: a -> Text
 
+
+class ZeptoCdnUrl a where
+    urlZeptoJsText :: a -> Text
+
+class OnePageScrollCssCdnUrl a where
+    urlOnePageScrollCssText :: a -> Text
+
+class OnePageScrollJsJqueryCdnUrl a where
+    urlOnePageScrollJsJqueryText :: a -> Text
+
+
+data BootcssCdn = BootcssCdn Bool
 
 instance JqueryCdnUrl BootcssCdn where
     urlJqueryJsText (BootcssCdn min_ver) =
@@ -30,3 +40,21 @@ instance JqueryCdnUrl BootcssCdn where
         if min_ver
             then "//cdn.bootcss.com/jqueryui/1.11.4/jquery-ui.min.css"
             else "//cdn.bootcss.com/jqueryui/1.11.4/jquery-ui.css"
+
+instance ZeptoCdnUrl BootcssCdn where
+    urlZeptoJsText (BootcssCdn min_ver) =
+        if min_ver
+            then "//cdn.bootcss.com/zepto/1.1.6/zepto.min.js"
+            else "//cdn.bootcss.com/zepto/1.1.6/zepto.js"
+
+instance OnePageScrollCssCdnUrl BootcssCdn where
+    urlOnePageScrollCssText (BootcssCdn min_ver) =
+        if min_ver
+            then "//cdn.bootcss.com/onepage-scroll/1.3.1/onepage-scroll.min.css"
+            else "//cdn.bootcss.com/onepage-scroll/1.3.1/onepage-scroll.css"
+
+instance OnePageScrollJsJqueryCdnUrl BootcssCdn where
+    urlOnePageScrollJsJqueryText (BootcssCdn min_ver) =
+        if min_ver
+            then "//cdn.bootcss.com/onepage-scroll/1.3.1/jquery.onepage-scroll.min.js"
+            else "//cdn.bootcss.com/onepage-scroll/1.3.1/jquery.onepage-scroll.js"
