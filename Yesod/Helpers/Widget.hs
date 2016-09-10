65a85fd466565a43097ab55e3c459f1afa13f2a6
Gilbert Fine <gilbert.fine@gmail.com>
2016-09-10 18:21:28 +0800

    new function: simpleFormPageWidget

--- Yesod/Helpers/Widget.hs
+++ Yesod/Helpers/Widget.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Yesod.Helpers.Widget where
 
-import Prelude
-import Yesod
-
-import Data.List                            (intersperse)
+import ClassyPrelude.Yesod
 
 import Yesod.Helpers.Message
+import Yesod.Helpers.Form2
 
 
 -- | construct page title by merging pieces of texts, separated by MsgPageTitleSep
@@ -20,3 +20,28 @@ mergePageTitles parts = do
     mr <- getMessageRender
     mr2 <- getMessageRender
     setTitleI $ mconcat $ intersperse (mr MsgPageTitleSep) $ map mr2 parts
+
+
+-- | A simple widget to show a html form
+simpleFormPageWidget :: (MonadIO m, MonadThrow m, ToWidget master w
+                        , MonadBaseControl IO m
+                        , RenderMessage master a
+                        , RenderMessage master YHCommonMessage
+                        )
+                     => (w, Enctype)
+                     -> Route master  -- ^ action url
+                     -> Maybe a       -- ^ any global error message
+                     -> FieldErrors master  -- ^ error messages for form fields
+                     -> WidgetT master m ()
+simpleFormPageWidget (formWidget, formEnctype) action m_err_msg form_errs = do
+  [whamlet|
+$maybe err_msg <- m_err_msg
+   <div .form-group>
+     <span .err_msg>_{err_msg}
+<ul>
+  $forall ((_name, fs), errs) <- fieldErrorsToList form_errs
+    <li>_{fsLabel fs}: #{intercalate ";" errs}
+<form method=post action="@{action}" enctype=#{formEnctype}>
+  ^{formWidget}
+  <input .btn .btn-primary type=submit value=_{MsgSubmitForm}>
+  |]
