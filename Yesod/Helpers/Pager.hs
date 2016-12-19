91fa7ceb7e5ea7f1a553a45c068261e605db7e27
Gilbert Fine <gilbert.fine@gmail.com>
2016-12-19 10:55:33 +0800

    change pager api
    Gettings current page number is the job of another function, not
    runPager

--- Yesod/Helpers/Pager.hs
+++ Yesod/Helpers/Pager.hs
@@ -25,25 +25,38 @@ data PagedResult = PagedResult
 jsonPagedResult :: PagedResult -> Value
 jsonPagedResult x = object [ "first_page_url" .= pagedFirstPageUrl x
                            , "last_page_url" .= pagedLastPageUrl x
+                           , "current_page_num" .= pagedCurrentPageNum x
                            , "last_page_num" .= pagedLastPageNum x
                            , "prev_page_url" .= pagedPrevPageUrl x
                            , "next_page_url" .= pagedNextPageUrl x
                            ]
 
 
-runPager :: (MonadHandler m, RenderMessage (HandlerSite m) FormMessage)
+pagerGetCurrentPageNumGet :: (MonadHandler m, RenderMessage (HandlerSite m) FormMessage) => PagerSettings -> m Int
+pagerGetCurrentPageNumGet (PagerSettings { pagerPageNumParam = pn_param }) =
+  fmap (max 1 . fromMaybe 1) $ runInputGet $ iopt intField pn_param
+
+
+pagerGetCurrentPageNumPost :: (MonadHandler m, RenderMessage (HandlerSite m) FormMessage) => PagerSettings -> m Int
+pagerGetCurrentPageNumPost (PagerSettings { pagerPageNumParam = pn_param }) =
+  fmap (max 1 . fromMaybe 1) $ runInputPost $ iopt intField pn_param
+
+pagerGetOffset :: PagerSettings -> Int -> Int
+pagerGetOffset (PagerSettings { pagerNumPerPage = npp } ) pn = (pn -1) * npp
+
+runPager :: (MonadHandler m)
          => PagerSettings
+         -> Int
          -> Int
          -> m PagedResult
 -- {{{1
-runPager (PagerSettings npp pn_param) total_num = do
+runPager (PagerSettings npp pn_param) pn total_num = do
   params0 <- reqGetParams <$> getRequest
   let params = foldr deleteMap params0 exclude_params
 
   current_route <- fromMaybe (error "getCurrentRoute failed") <$> getCurrentRoute
 
   url_render <- getUrlRenderParams
-  pn <- fmap (max 1 . fromMaybe 1) $ runInputGet $ iopt intField pn_param
 
   let link_to_pn p = url_render current_route $ insertMap pn_param (tshow p) params
       first_page_url = link_to_pn (1 :: Int)
