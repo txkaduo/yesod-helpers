e2cda61ae3446f66af5ce4bfc6b00e394f9ed146
Gilbert Fine <gilbert.fine@gmail.com>
2017-09-18 00:07:16 +0800

    derivePathPieceS has been moved and refactored

--- Yesod/Helpers/Parsec.hs
+++ Yesod/Helpers/Parsec.hs
@@ -11,29 +11,3 @@ import Control.Monad
 import Data.Text                            (Text)
 
 import Text.Parsec.TX.Utils
-
-derivePathPieceS :: String -> Q [Dec]
-derivePathPieceS s = do
-    to_pathpiece <- [| toPathPiece . simpleEncode |]
-    from_pathpiece <- [| join . fmap (parseWithCharParserMaybe simpleParser) . (fromPathPiece :: Text -> Maybe Text) |]
-    return
-        [ pathPieceInstanceD (ConT $ mkName s)
-            [ FunD 'toPathPiece
-                [ Clause [] (NormalB to_pathpiece) []
-                ]
-            , FunD 'fromPathPiece
-                [ Clause [] (NormalB from_pathpiece) []
-                ]
-            ]
-        ]
-
-
-#if MIN_VERSION_template_haskell(2, 11, 0)
-#define NO_OVERLAP Nothing
-#else
-#define NO_OVERLAP
-#endif
-
-pathPieceInstanceD :: Type -> [Dec] -> Dec
-pathPieceInstanceD typ =
-    InstanceD NO_OVERLAP [] (ConT ''PathPiece `AppT` typ)
