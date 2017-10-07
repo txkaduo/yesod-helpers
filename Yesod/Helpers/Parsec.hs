f64d1ae56f6a88e4cf8f48e1b3cc00d0b963f20b
Gilbert Fine <gilbert.fine@gmail.com>
2017-10-07 12:18:03 +0800

    trivial: suppress some warnings

--- Yesod/Helpers/Parsec.hs
+++ Yesod/Helpers/Parsec.hs
@@ -1,13 +1,7 @@
 module Yesod.Helpers.Parsec
-    ( module Yesod.Helpers.Parsec
-    , module Text.Parsec.TX.Utils
+    ( module Text.Parsec.TX.Utils
     )
     where
 
-import Prelude
-import Yesod
-import Language.Haskell.TH
-import Control.Monad
-import Data.Text                            (Text)
 
 import Text.Parsec.TX.Utils
